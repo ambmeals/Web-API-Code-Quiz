911c3282e100528dceb8caee0550c114070c59ad
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-03 00:07:55 -0600

    UpdatedCode1.2

---
+++ node.js
@@ -0,0 +1,11 @@
+var Question1 = Q1;
+var Question2 = Q2;
+var Question3 = Q3;
+var Question4 = Q4;
+var Question5 = Q5;
+var Question6 = Q6;
+var Question7 = Q7;
+var Question8 = Q8;
+var Question9 = Q9;
+var Question10 = Q10;
+
