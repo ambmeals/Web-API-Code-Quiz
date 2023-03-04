8012f552b0c408ba07bb627aca2d74fb1472b748
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-04 12:46:08 -0600

    Update the code 1.4

--- node.js
+++ node.js
@@ -1,11 +1,6 @@
-var Question1 = Q1;
-var Question2 = Q2;
-var Question3 = Q3;
-var Question4 = Q4;
-var Question5 = Q5;
-var Question6 = Q6;
-var Question7 = Q7;
-var Question8 = Q8;
-var Question9 = Q9;
-var Question10 = Q10;
+const startButton = document.getElementById('start-button');
 
+startButton.addEventListener('click', function() {
+  alert('The quiz has started!');
+  startButton.style.backgroundColor = "red";
+});
